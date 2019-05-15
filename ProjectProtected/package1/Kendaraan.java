43b1e272a881f78a44c74f93286f8b30b644ba22
fachrulpbm <fachrul.pbm@polindra.ac.id>
2019-05-16 00:10:04 +0700

    Project Protected

---
+++ ProjectProtected/package1/Kendaraan.java
@@ -0,0 +1,5 @@
+public class Kendaraan{
+
+	protected String merk;
+	
+}
---
+++ ProjectProtected/package2/Mobil.java
@@ -0,0 +1,8 @@
+public class Mobil extends Kendaraan{
+
+	public static void main(String[] args) {
+        Mobil m = new Mobil();
+        m.merk = "Toyota";
+    }
+
+}
